f2b5f832b515af610290ed6c33cd0f045e41938b
Umair Rasheed <34655240+umair3082@users.noreply.github.com>
2021-08-24 16:16:29 +0500

    Create commoncode.cs
    common code class created

---
+++ commoncode.cs
@@ -0,0 +1,17 @@
+public BindingSource bindingSource1 = new BindingSource();
+        public  DataTable GetData(string sqlCommand,string connectionString)
+        {
+            
+
+            SqlConnection con = new SqlConnection(connectionString);
+        
+            SqlCommand command = new SqlCommand(sqlCommand, connection.con);
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            adapter.SelectCommand = command;
+
+            DataTable table = new DataTable();
+            table.Locale = System.Globalization.CultureInfo.InvariantCulture;
+            adapter.Fill(table);
+
+            return table;
+        }
